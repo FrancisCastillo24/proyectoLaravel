29f13166d51ae6bf5cfab71edbbc4394ef7cd125
FrancisCastillo24 <castillofernandezfrancis@gmail.com>
2024-06-21 00:21:32 +0200

    Reutilización formulario
    He arreglado el tema de formulario reutilizable para que se pueda usar tanto en editar como crear en el mismo formulario.

--- resources/views/pelicula/form.blade.php
+++ resources/views/pelicula/form.blade.php
@@ -1,23 +1,26 @@
 <!--Formulario que tendrá los datos en común con create y edit-->
 
 <!--FORMULARIO REUTILIZABLE-->
+<!--Al ser un formulario reutilizable, cuando editemos nos muestra su información original, pero si creamos no debe mostrarlo, por lo que nos aseguramos de que exista o no-->
 <label for="Title">Nombre de la película</label>
-<input type="text" name="Title" value="{{$pelicula->Title}}" id="Title"><br><br>
+<input type="text" name="Title" value="{{isset($pelicula->Title) ? $pelicula->Title : ''}}" id="Title"><br><br>
 
 <label for="Description">Descripción de la película</label>
-<input type="text" name="Description" value="{{$pelicula->Description}}" id="Description"><br><br>
+<input type="text" name="Description" value="{{isset($pelicula->Description) ? $pelicula->Description : ''}}" id="Description"><br><br>
 
 <label for="Release_date">Fecha lanzamiento de la película</label>
-<input type="date" name="Release_date" value="{{$pelicula->Release_date}}" id="Release_date"><br><br>
+<input type="date" name="Release_date" value="{{isset($pelicula->Release_date) ? $pelicula->Release_date : ''}}" id="Release_date"><br><br>
 
 <label for="Runtime">Duración de la película</label>
-<input type="number" name="Runtime" value="{{$pelicula->Runtime}}" id="Runtime"><br><br>
+<input type="number" name="Runtime" value="{{isset($pelicula->Runtime) ? $pelicula->Runtime : ''}}" id="Runtime"><br><br>
 
 <label for="Director">Director de la película</label>
-<input type="text" name="Director" value="{{$pelicula->Director}}" id="Director"><br><br>
+<input type="text" name="Director" value="{{isset($pelicula->Director) ? $pelicula->Director : ''}}" id="Director"><br><br>
 
 <label for="Photo">Añadir foto</label>
+@if(isset($pelicula->Photo))
 <img src="{{asset('storage').'/'.$pelicula->Photo}}" width="100" alt="">
+@endif
 <input type="file" name="Photo" value="" id="Photo"><br><br>
 
 <input type="submit" value="Guardar datos">

17db44e48f4a5ae25e722f5f966f0a18e36ef265
FrancisCastillo24 <castillofernandezfrancis@gmail.com>
2024-06-21 02:01:49 +0200

    Redireccionar rutas
    Si te has autenticado o no, te dirige a una ruta u otra.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PeliculaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 /* Accedemos a la carpeta empleado de la vista
@@ -20,4 +20,9 @@ Route::get('pelicula/create', [PeliculaController::class, 'create']);
 Route::resource('pelicula', PeliculaController::class);
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Te lleva a home
+Route::get('/home', [PeliculaController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/', [PeliculaController::class, 'index'])->name('home');
+});
